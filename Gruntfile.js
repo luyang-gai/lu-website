6a3f7fa69a5034ab8fdd2bf1162031888a053fc4
gail <luyang.gai@agilysys.com>
2015-11-04 19:27:08 -0800

    live reload working

--- Gruntfile.js
+++ Gruntfile.js
@@ -24,32 +24,43 @@ module.exports = function(grunt) {
       }
     },
     connect: {
-      example: {
-        port: 8000,
-        base: 'app',
-        keepalive: true,
-        livereload: true
+      server: {
+        options: {
+          livereload: true,
+          port: 8000,
+          base: 'app'
+        },
       }
     },
     watch: {
-      scripts: {
+      options: {
+        livereload: true,
+      },
+      html: {
+        files: ['app/index.html',
+                'app/js/**/*.html']
+      },
+      sass: {
+        options: {
+          livereload: false,
+        },
         files: ['app/css/app.scss'],
         tasks: ['sass'],
-        options: {
-          livereload: true,
-          spawn: false,
-        },
-      },
+      }
     }
   });
 
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-sass');
-  grunt.loadNpmTasks('grunt-connect');
+  grunt.loadNpmTasks('grunt-contrib-connect');
   grunt.loadNpmTasks('grunt-contrib-watch');
 
   // Default task(s).
-  grunt.registerTask('default', ['sass', 'connect:example']);
+  grunt.registerTask('serve', [
+    'sass',
+    'connect',
+    'watch'
+  ]);
 
 };
